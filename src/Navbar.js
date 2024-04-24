957ff5138698710018e0834bf92c0ab982f2a93e
Nishant Shinde <shindenishant200@gmail.com>
2024-04-24 20:19:18 +0530

    first push

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <navbar class="navbar">
+                <img src="./logo.svg"></img>
+                GeekFoods
+                <p class="topmid">Home
+                    Quote
+                    Restorants
+                    Foods
+                    Contacts</p>
+                <button>Get Started</button>
+            </navbar>
+        </div>
+    )
+}
